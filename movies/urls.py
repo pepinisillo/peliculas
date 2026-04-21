345b5a55f467054e4b03396637181a667ab285b0
VictorMML90 <l22211602@tectijuana.edu.mx>
2026-04-21 01:14:05 -0700

    Implementación de búsqueda de usuarios y funcionalidad 'Sorpréndeme'

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('all/', all_movies),
     path('search/', search, name='search'),
+    path('random/', random_movie, name='random_movie'),
     path('<int:movie_id>/', movie, name='pelis'),
     path('movie_comment/add/<int:movie_id>/', add_comment, name='add_comment'),
     path('add_comment/<int:movie_id>/', add_comment),
